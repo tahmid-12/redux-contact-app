b9854dd682e99c8f289e945408d3fe83bd33e1dd
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-12 01:15:40 +0600

    Validation in progress.

--- src/redux/reducers/ContactReducer.js
+++ src/redux/reducers/ContactReducer.js
@@ -2,12 +2,14 @@ const initialState = [
     {
         id: 0,
         name: "Robert Downey Jr",
-        number: 1234567890
+        number: 1234567890,
+        email: "robert@gmail.com"
     },
     {
         id: 1,
         name: "Chris Evans",
-        number: 9876543210
+        number: 9876543210,
+        email: "chris@gmail.com"
     },
 ];
 

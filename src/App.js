756258bfec281951161ffeae85b8760719c7a466
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2022-06-11 14:06:12 +0600

    Routing fixed for urls.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <h1>Welcome to Contact Book</h1>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<h1>Welcome to Contact Book</h1>} />
+        <Route path="/add" element={<h1>Add Contact</h1>} />
+        <Route path="/edit/:id" element={<h1>Edit Contact</h1>}/>
+      </Routes>
     </div>
   );
 }
